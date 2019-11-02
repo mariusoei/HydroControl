96902e3fd197acebc1452276269554e3f1944cbe
Marius Oei <marius.oei@isys.uni-stuttgart.de>
2019-11-02 18:52:56 +0100

    Remove obsolete and unnecessary functions from header

--- include/temperatureLogger.h
+++ include/temperatureLogger.h
@@ -3,12 +3,6 @@
 
 void setupTemperatureLogger();
 
-bool checkMeasurementPlausibility(float temperature_C);
-float sensorCalibration(float temperature_raw);
-void reconnect();
-bool reconnectOnce();
-void setup_wifi();
-
 void publishWaterTemperature();
 void measureWaterTemperature();
 void updateOledDisplay();
