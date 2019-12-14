10c20bfbc1ed9266f7fdca7cae8cf7b3f3b0faa8
Marius Oei <marius.oei@isys.uni-stuttgart.de>
2019-12-14 17:33:10 +0100

    Use WiFiManager instead of hardcoded credentials

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,3 @@
-// #define _TASK_TIMECRITICAL      // Enable monitoring scheduling overruns
-// #define _TASK_SLEEP_ON_IDLE_RUN // Enable 1 ms SLEEP_IDLE powerdowns between tasks if no callback methods were invoked during the pass
-// #define _TASK_STATUS_REQUEST    // Compile with support for StatusRequest functionality - triggering tasks on status change events in addition to time only
-// #define _TASK_WDT_IDS           // Compile with support for wdt control points and task ids
-// #define _TASK_LTS_POINTER       // Compile with support for local task storage pointer
-// #define _TASK_PRIORITY          // Support for layered scheduling priority
-// #define _TASK_MICRO_RES         // Support for microsecond resolution
-// #define _TASK_STD_FUNCTION      // Support for std::function (ESP8266 and ESP32 ONLY)
-// #define _TASK_DEBUG             // Make all methods and variables public for debug purposes
-// #define _TASK_INLINE            // Make all methods "inline" - needed to support some multi-tab, multi-file implementations
-// #define _TASK_TIMEOUT           // Support for overall task timeout
-// #define _TASK_OO_CALLBACKS      // Support for dynamic callback method binding
 #include <TaskScheduler.h>
 
 #include "SensorLogger.h"
@@ -17,6 +5,10 @@
 
 #include "PumpController.h"
 
+// For WifiManager library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 #define WIFI_CONNECTION_TIMEOUT 20
 const char* ssid = "***REMOVED***";
@@ -63,7 +55,9 @@ void setup_wifi() {
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    setup_wifi();
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("AutoConnectAP_HydroControl");
+    //setup_wifi();
     setupLogger();
     delay(100);
     Serial.println("Scheduler: setup complete");
