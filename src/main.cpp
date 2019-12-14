9e3e328d327654bd3485e2b49bebcc11070cfffc
Marius Oei <marius.oei@isys.uni-stuttgart.de>
2019-12-14 17:38:40 +0100

    Remove obsolete Wifi code

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
-#define WIFI_CONNECTION_TIMEOUT 20
-const char* ssid = "***REMOVED***";
-const char* password = "***REMOVED***";
-
 #define PIN_DIR D2
 #define PIN_STEP D3
 #define PIN_SLEEP D4
@@ -28,36 +24,12 @@ Scheduler ts;
 
 
 
-void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while(WiFi.status() != WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
     WiFiManager wifiManager;
     wifiManager.autoConnect("AutoConnectAP_HydroControl");
-    //setup_wifi();
     setupLogger();
     delay(100);
     Serial.println("Scheduler: setup complete");
